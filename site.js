46a692cf3cbee1e95818cba17f99381fdb4f7d48
Brad McQueary <Bradley.McQueary@gmail.com>
2021-11-08 19:46:35 -0500

    JavaScript added

---
+++ site.js
@@ -0,0 +1,3 @@
+document.getElementById("show").onclick = function () {
+    location.href = "https://www.facebook.com/events/469627797734251?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22your_upcoming_events_unit%22%2C%22surface%22%3A%22bookmark%22%7D]%2C%22ref_notif_type%22%3Anull%7D";
+};
